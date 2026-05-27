4cc6d835296e3939be3f4894a82dfe1cd600b7fc
Nisarg1223 <darjinisarg49@gmail.com>
2026-05-27 21:02:59 +0530

    OverworldMap added

--- Overworld.js
+++ Overworld.js
@@ -3,38 +3,38 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.map=null;
   }
+startGameLoop(){
+  
+  const step=()=>{
 
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+    //Draw Lower Layer
+    this.map.drawLowerImage(this.ctx);
+      Object.values(this.map.gameObjects).forEach(object=>{
+      
+        object.sprite.draw(this.ctx)
+      })
+     //Draw Upper Layer
+    this.map.drawUpperImage(this.ctx);
+
+
+    // console.log("stepping!!")
+   requestAnimationFrame(()=>{
+
+    step();
+   })
+  }
+  step();
+}
   init() {
+    this.map = new OverworldMap(
+      window.OverworldMaps.Kitchen
+    )
+   this.startGameLoop();
+ 
 
-    // Map image
-    const image = new Image();
-
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-
-    image.src = "/images/maps/Demolower.png";
-
-  //place some game objects.
-  const hero = new GameObject({
-    x:5,
-    y:6,
-
-  }) 
-
-  const npc1 = new GameObject({
-    x:7,
-    y:9,
-    src:"/images/characters/people/npc1.png"
-  })
-  setTimeout(()=>{
-    hero.sprite.draw(this.ctx);
-  npc1.sprite.draw(this.ctx);
-  },200)
-  
-
-  
 
 
   }
---
+++ OverworldMap.js
@@ -0,0 +1,60 @@
+class OverworldMap {
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
+
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc;
+
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+  }
+  drawLowerImage(ctx) {
+    ctx.drawImage(this.lowerImage, 0, 0);
+  }
+  drawUpperImage(ctx) {
+    ctx.drawImage(this.upperImage, 0, 0);
+  }
+}
+
+window.OverworldMaps = {
+  DemoRoom: {
+    lowerSrc: "/images/maps/DemoLower.png",
+    upperSrc: "/images/maps/Demoupper.png",
+    gameObjects: {
+      hero: new GameObject({
+        x: 5,
+        y: 6,
+      }),
+      npc1: new GameObject({
+        x: 7,
+        y: 9,
+        src: "/images/characters/people/npc1.png",
+      }),
+    },
+  },
+  Kitchen: {
+    lowerSrc: "/images/maps/KitchenLower.png",
+    upperSrc: "/images/maps/KitchenUpper.png",
+    gameObjects: {
+      hero: new GameObject({
+        x: 3,
+        y: 5,
+      }),
+      npc1: new GameObject({
+        x: 3,
+        y: 8,
+        src: "/images/characters/people/npc1.png",
+      }),
+      npc2: new GameObject({
+        x: 7,
+        y: 9,
+        src: "/images/characters/people/npc2.png",
+      }),
+      npc3: new GameObject({
+        x: 2,
+        y: 6,
+        src: "/images/characters/people/npc4.png",
+      }),
+    },
+  },
+};
